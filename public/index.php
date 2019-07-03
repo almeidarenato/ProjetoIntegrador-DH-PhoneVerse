3dcd33306fb9d16b45cc33483a8b21a73f440207
almeidarenato <renatoam@ymail.com>
2019-07-03 02:02:48 -0300

    including sprint1 data

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Split Page CSS & JS -->
+  <link rel="stylesheet" href="assets/styles/split.css">
+  <script defer src='assets/scripts/split.js'></script>
+
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css?family=Titillium+Web:600&display=swap" rel="stylesheet">
+
+  <!-- Bootstrap CSS & JS -->
+
+  <title>PhoneVerse</title>
+</head>
+
+<body>
+  <nav>
+    <span id="logo">PhoneVerse</span>
+    <span>O fim para a dúvida na escolher do seu smartphone</span>
+    <span></span>
+  </nav>
+  <div class="container">
+    <div class="split left">
+      <h1>Me ajude a escolher</h1>
+      <a href="#" class="button">Vamos lá!</a>
+    </div>
+    <div class="split right">
+      <h1>Eu sei exatamente<br> oque preciso!</h1>
+      <a href="#" class="button">Me mostre as opções!</a>
+    </div>
+  </div>
+</body>
+
+</html>
