7000aa9c8f4f60e7353867927d23585c643abd1f
Raul <raulrangel.ss91@gmail.com>
2019-08-08 22:56:42 -0300

    commit inicial

---
+++ public/Squadfree/inc/simple-register-form/simple-register-form/README.markdown
@@ -0,0 +1,11 @@
+# Simple register form
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/arcs/pen/OmZaex](https://codepen.io/arcs/pen/OmZaex).
+
+   Thanks for checking out this pen.
+  
+  Credits for the design to XavierCoulombeM
+  @ https://dribbble.com/shots/2510592-Simple-register-form
+  
+  This Pen has no libraries except fonts
+  
+  The answers are stored in the `questions` array
---
+++ public/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Split Page CSS & JS -->
+  <link rel="stylesheet" href="assets/styles/split.css">
+  <script defer src='assets/scripts/split.js'></script>
+
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css?family=Titillium+Web:600&display=swap" rel="stylesheet">
+
+  <!-- Bootstrap CSS & JS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <title>PhoneVerse</title>
+</head>
+
+<body>
+  <nav>
+
+  </nav>
+  <div class="container col-md-12">
+    <div class="split left d-flex">
+      <h1>Me ajude a escolher</h1>
+      <p>Deste lado, nós te mostraremos as melhores opções de aparelhos com base em suas respostas em um rápido questionário </p>
+
+      <div class="typeform-widget" data-url="https://raulrangel489504.typeform.com/to/GwaahJ" style="width: 50%; height: 350px;"></div>
+      <script>
+        (function() {
+          var qs, js, q, s, d = document,
+            gi = d.getElementById,
+            ce = d.createElement,
+            gt = d.getElementsByTagName,
+            id = "typef_orm",
+            b = "https://embed.typeform.com/";
+          if (!gi.call(d, id)) {
+            js = ce.call(d, "script");
+            js.id = id;
+            js.src = b + "embed.js";
+            q = gt.call(d, "script")[0];
+            q.parentNode.insertBefore(js, q)
+          }
+        })()
+      </script>
+    </div>
+    <div class="split right">
+      <h1>Eu sei exatamente<br>oque preciso!</h1>
+      <h2> Já Deste lado, você poderá escolher cada detalhe especifico dos aparelhos que deseja ver. </h2>
+    </div>
+  </div>
+</body>
