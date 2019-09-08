0058cedb177093282e6695104777964d816bf6bd
almeidarenato <renatoam@ymail.com>
2019-09-08 10:17:01 -0300

    criacao da migration da tabela users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,10 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->date('dtNascimento');
+            $table->string('aparelho');
+            $table->string('motivo');
+            $table->string('usoCelular');
         });
     }
 
