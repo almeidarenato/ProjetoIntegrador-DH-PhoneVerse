eb540f1b2fc1a6b882d93462074baf647b9fe2a6
almeidarenato <renatoam@ymail.com>
2019-09-10 01:19:04 -0300

    finalização do ajuste de pastas

--- app/Http/Controllers/QuestionarioController.php
+++ app/Http/Controllers/QuestionarioController.php
@@ -13,47 +13,63 @@ class QuestionarioController extends Controller
     public function cadastrar(Request $request)
     {
         //var_dump($request->input('dtNascimento'));
+        $opcao = $request->input('opcaousuario');
+        if ($opcao == 'esquerda') {
 
-        //echo $date;
-        //exit;
-        //var_dump($date);
-        // $date =  date('d/M/Y h:i:s', $date);
-        //var_dump($date);
-        //$motivo
-        $naoquero = $request->input('naoQuero');
-        if (isset($naoquero)) {
-            $respostas = [
-                'nome' => $request->input('nome'),
-                'dtNascimento' => $request->input('dtNascimento'),
-                'aparelho' => $request->input('aparelho'),
-                'motivo' => $request->input('resp3'),
-                'usoCelular' => $request->input('resp4')
-            ];
-            $user = User::create([
-                'nome' => $request->input('nome'),
-                'email' => $request->input('email'),
-                'senha' => $request->input('senha'),
-                'dtNascimento' => $request->input('dtNascimento'),
-                'aparelho' => $request->input('aparelho'),
-                'motivo' => $request->input('resp3'),
-                'usoCelular' => $request->input('resp4')
-            ]);
-        } else {
+            //echo $date;
+            //exit;
+            //var_dump($date);
+            // $date =  date('d/M/Y h:i:s', $date);
+            //var_dump($date);
+            //$motivo
+            $naoquero = $request->input('naoQuero');
+            if (isset($naoquero)) {
+                $respostas = [
+                    'nome' => $request->input('nome'),
+                    'dtNascimento' => $request->input('dtNascimento'),
+                    'aparelho' => $request->input('aparelho'),
+                    'motivo' => $request->input('resp3'),
+                    'usoCelular' => $request->input('resp4')
+                ];
+            } else {
 
-            $request->validate([
-                'email' => 'required|unique:users',
-                'senha' => 'required',
-                'dtNascimento' => 'required'
-            ]);
-            //$email = $request->input('email');
-            // $verifica = User::whereEmail($email)->get();
+                $request->validate([
+                    'email' => 'required|unique:users',
+                    'senha' => 'required',
+                    'dtNascimento' => 'required'
+                ]);
+                //$email = $request->input('email');
+                // $verifica = User::whereEmail($email)->get();
 
-            /* if (isset($verifica[0]->email)) {
+                /* if (isset($verifica[0]->email)) {
                 echo "seu e-mail já está cadastrado";
             } else {
                 echo "oba seu email é novo";
                 exit;
                 */
+                $respostas = [
+                    'nome' => $request->input('nome'),
+                    'email' => $request->input('email'),
+                    'senha' => $request->input('senha'),
+                    'dtNascimento' => $request->input('dtNascimento'),
+                    'aparelho' => $request->input('aparelho'),
+                    'motivo' => $request->input('resp3'),
+                    'usoCelular' => $request->input('resp4')
+                ];
+
+                $user = User::create([
+                    'name' => $request->input('nome'),
+                    'email' => $request->input('email'),
+                    'password' => Hash::make($request->input('senha')),
+                    'dtNascimento' => $request->input('dtNascimento'),
+                    'aparelho' => $request->input('aparelho'),
+                    'motivo' => $request->input('resp3'),
+                    'usoCelular' => $request->input('resp4')
+                ]);
+            }
+            //echo $respostas['email'];
+            //var_dump($respostas);
+        } else {
             $respostas = [
                 'nome' => $request->input('nome'),
                 'email' => $request->input('email'),
@@ -63,19 +79,7 @@ class QuestionarioController extends Controller
                 'motivo' => $request->input('resp3'),
                 'usoCelular' => $request->input('resp4')
             ];
-            $user = User::create([
-                'name' => $request->input('nome'),
-                'email' => $request->input('email'),
-                'password' => Hash::make($request->input('senha')),
-                'dtNascimento' => $date,
-                'aparelho' => $request->input('aparelho'),
-                'motivo' => $request->input('resp3'),
-                'usoCelular' => $request->input('resp4')
-            ]);
-            /*}*/
         }
-        //echo $respostas['email'];
-        //var_dump($respostas);
         $user->save();
         return view('respostas')->with('respostas', $respostas);
     }
--- public/frontend/contactform/Readme.txt
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-Fully working PHP/AJAX contact form is available in the pro version.
-You can buy it from: https://bootstrapmade.com/squadfree-free-bootstrap-template-creative/
-=======
-Fully working PHP/AJAX contact form is available in the pro version.
-You can buy it from: https://bootstrapmade.com/squadfree-free-bootstrap-template-creative/
->>>>>>> master
