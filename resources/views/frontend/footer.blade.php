e04e8b107aafdcf66dc6bdddd6c500c9588c6590
Diogo Costa <diogo.costa.d8c@gmail.com>
2019-09-13 22:33:10 -0300

    Criacao_paginaTop3

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -3,11 +3,11 @@
         <div class="row">
             <div class="col-md-12 col-lg-12">
                 <div class="wow shake" data-wow-delay="0.4s">
-                    <div class="page-scroll marginbot-30">
+                    {{-- <div class="page-scroll marginbot-30">
                         <a href="#intro" id="totop" class="btn btn-circle">
                             <i class="fa fa-angle-double-up animated"></i>
                         </a>
-                    </div>
+                    </div> --}}
                 </div>
                 <p>&copy;PhoneVerse.</p>
                 <div class="credits">
---
+++ resources/views/header2.blade.php
@@ -0,0 +1,82 @@
+{{-- <link rel="stylesheet" href="css/style.css">
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.png" alt="" srcset="" class='logo'>
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        
+        
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#intro">Home- test2</a></li>
+                <li><a href="#about">Sobre</a></li>
+                <li><a href="#service">Seu Smartphone</a></li>
+                <li><a href="#contact">Contato</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Meu Cadastro<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Meus Dados</a></li>
+                        <li><a href="#">Meus favoritos</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav> --}}
+
+
+{{-- <link rel="stylesheet" href="css/style.css"> --}}
+<nav class="navbar navbar-expand-lg navbar-light bg-light" >
+        <a class="navbar-brand navbar-right" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider" style="color:blue"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+          {{-- <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form> --}}
+        </div>
+      </nav>
+
+
