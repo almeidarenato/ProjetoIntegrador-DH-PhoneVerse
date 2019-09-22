1dfef47be25e7943249436c628984c590a46f4b8
almeidarenato <renatoam@ymail.com>
2019-09-22 16:18:33 -0300

    criacao do crud memoriainterna

--- app/Http/Controllers/memoriaInternaController.php
+++ app/Http/Controllers/memoriaInternaController.php
@@ -7,55 +7,61 @@ use App\MemoriaInterna;
 
 class memoriaInternaController extends Controller
 {
-        public function listandoMemoriaInterna(){
-            $memoriaInterna = MemoriaInterna::all();
-    
-            return view('listandoMemoriaInterna')->with('memoriaInterna', $memoriaInterna);
-        }
-    
-        public function adicionandoMemoriaInterna(){
-            return view('adicionandoMemoriaInterna');
-        }
-    
-        public function salvandoMemoriaInterna(Request $request){
-            $request->validate([
-                "quantidade" => "required|max:50"
-            ]);
-    
-            $memoriaInterna = MemoriaInterna::create([
-                "quantidade" => $request->input('quantidade')
-            ]);
-    
-            $memoriaInterna->save();
-    
-            return redirect('/memoriainterna');
-        }
+    public function listandoMemoriaInterna()
+    {
+        $memoriaInterna = MemoriaInterna::orderBy('id', 'ASC')->paginate(5);
 
-        public function alterandoMemoriaInterna($id){
-            $memoriaInterna = MemoriaInterna::find($id);
-    
-            return view('adicionandoMemoriaInterna')->with('memoriaInterna', $memoriaInterna);
-        }
-    
-        public function modificandoMemoriaInterna(Request $request, $id){
-            $memoriaInterna = MemoriaInterna::find($id);
-    
-            $request->validate([
-                "quantidade" => "required|max:200"
-            ]);
-    
-            $memoriaInterna->quantidade = $request->input('quantidade');
-    
-            $memoriaInterna->save();
-    
-            return redirect('/memoriainterna');
-        }
-    
-        public function removendoMemoriaInterna($id){
-            $memoriaInterna = MemoriaInterna::find($id);
-    
-            $memoriaInterna->delete();
-    
-            return redirect('/memoriainterna');
-        }
+        return view('admin.memoriainterna')->with('memoriaInterna', $memoriaInterna);
+    }
+
+    public function adicionandoMemoriaInterna()
+    {
+        return view('adicionandoMemoriaInterna');
+    }
+
+    public function salvandoMemoriaInterna(Request $request)
+    {
+        $request->validate([
+            "quantidade" => "required|max:50"
+        ]);
+
+        $memoriaInterna = MemoriaInterna::create([
+            "quantidade" => $request->input('quantidade')
+        ]);
+
+        $memoriaInterna->save();
+
+        return redirect('/admin/memint/salvo');
+    }
+
+    public function alterandoMemoriaInterna($id)
+    {
+        $memoriaInterna = MemoriaInterna::find($id);
+
+        return view('adicionandoMemoriaInterna')->with('memoriaInterna', $memoriaInterna);
+    }
+
+    public function modificandoMemoriaInterna(Request $request, $id)
+    {
+        $memoriaInterna = MemoriaInterna::find($id);
+
+        $request->validate([
+            "quantidade" => "required|max:200"
+        ]);
+
+        $memoriaInterna->quantidade = $request->input('quantidade');
+
+        $memoriaInterna->save();
+
+        return redirect('/admin/memint/editado');
+    }
+
+    public function removendoMemoriaInterna($id)
+    {
+        $memoriaInterna = MemoriaInterna::find($id);
+
+        $memoriaInterna->delete();
+
+        return redirect('/admin/memint/excluido');
+    }
 }
