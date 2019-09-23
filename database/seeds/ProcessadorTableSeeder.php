b7e0c9ae98db9f00144b13c9df2275c6bb0d9f5e
almeidarenato <renatoam@ymail.com>
2019-09-22 23:20:50 -0300

    correção do seeder processador

--- database/seeds/ProcessadorTableSeeder.php
+++ database/seeds/ProcessadorTableSeeder.php
@@ -22,20 +22,19 @@ class ProcessadorTableSeeder extends Seeder
         Processador::create(["id" => 8, "modelo" => "Snapdragon 425", "marca" => "Qualcomm"]);
         Processador::create(["id" => 9, "modelo" => "Snapdragon 636", "marca" => "Qualcomm"]);
         Processador::create(["id" => 10, "modelo" => "Snapdragon 425", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 11, "modelo" => "Snapdragon 636", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 12, "modelo" => "Snapdragon 801", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 13, "modelo" => "Snapdragon 835", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 14, "modelo" => "Snapdragon 845", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 15, "modelo" => "Snapdragon 660", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 16, "modelo" => "Snapdragon 626", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 17, "modelo" => "Snapdragon 630", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 18, "modelo" => "Snapdragon 665", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 19, "modelo" => "Snapdragon 675", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 20, "modelo" => "Snapdragon 821", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 21, "modelo" => "Snapdragon 855 plus", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 22, "modelo" => "Snapdragon 855", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 23, "modelo" => "Snapdragon 625", "marca" => "Qualcomm"]);
-        Processador::create(["id" => 24, "modelo" => "Exynons 7", "marca" => "Samsung"]);
-        Processador::create(["id" => 25, "modelo" => "Exynos 9", "marca" => "Samsung"]);
+        Processador::create(["id" => 11, "modelo" => "Snapdragon 801", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 12, "modelo" => "Snapdragon 835", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 13, "modelo" => "Snapdragon 845", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 14, "modelo" => "Snapdragon 660", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 15, "modelo" => "Snapdragon 626", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 16, "modelo" => "Snapdragon 630", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 17, "modelo" => "Snapdragon 665", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 18, "modelo" => "Snapdragon 675", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 19, "modelo" => "Snapdragon 821", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 20, "modelo" => "Snapdragon 855 plus", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 21, "modelo" => "Snapdragon 855", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 22, "modelo" => "Snapdragon 625", "marca" => "Qualcomm"]);
+        Processador::create(["id" => 23, "modelo" => "Exynons 7", "marca" => "Samsung"]);
+        Processador::create(["id" => 24, "modelo" => "Exynos 9", "marca" => "Samsung"]);
     }
 }
