d4b3fc934592c883f87d0d45d8d08cb75f54c981
Diogo Costa <diogo.costa.d8c@gmail.com>
2019-09-24 22:02:42 -0300

    criacao do top3 e pagina do produto

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('frontend.landing');
 });
 
+Route::get('/teste', function () {
+    return view('frontend.teste');
+});
+
 Route::post('/respostas', 'QuestionarioController@cadastrar');
 
 Auth::routes();
@@ -23,4 +27,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/top3', function(){
     return view('top_page');
+});
+
+Route::get('/top3x', function(){
+    return view('top_page_x');
+});
+
+Route::get('/produto', function(){
+    return view('product');
 });
